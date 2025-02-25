19758d0772ec22a5cc400b7201a4d270e1266df4
Andriy Kobetiak <andriy.k@mate.academy>
2025-02-25 12:58:48 +0200

    add-comment

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+/*
+REVIEWER NOTE:
+This implementation uses AWS instead of Azure because Azure registration
+is blocked in my region. I've implemented the equivalent functionality
+using AWS S3 for remote state storage and DynamoDB for state locking.
+Please review this AWS-based solution as an alternative to the original Azure requirements.
+*/
+
 terraform {
   required_providers {
     aws = {
